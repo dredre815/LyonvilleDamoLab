de47f397fa7a4f98e78a0c961944c6b9d83863d7
Xiaoyu Pang <1307738956@qq.com>
2023-09-22 19:59:26 +0800

    the design of real estate page

---
+++ Real Estate/src/App.jsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+import { RealEstate } from "./RealEstate/RealEstate";
+
+export default function App() {
+return (
+    <div>
+    <RealEstate />
+    </div>
+);
+}
--- lyonville-frontend-damolab/build/wrong-test.js
+++
@@ -1,8 +0,0 @@
-/**
- * @param {string} name
- */
-function greet(name) {
-    console.log("Hello, " + name);
-}
-greet(42);
-export {};
