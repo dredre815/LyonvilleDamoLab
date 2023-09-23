dd04d112d4e9e9e0b515e9307fa25121887dc745
Yingzhe Sun <yingzhe@student.unimelb.edu.au>
2023-09-23 15:10:25 +1000

    basic window

---
+++ Scroll Window/src/App.jsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+import { scroll } from "./scroll/scroll";
+
+export default function App() {
+return (
+    <div>
+    <scroll />
+    </div>
+);
+}
---
+++ Scroll Window/src/scroll/scroll.jsx
@@ -0,0 +1,165 @@
+
+import styles from "./MacBookPro1614.module.css";
+
+export const MacBookPro1614 = ({ ...props }) => {
+  return (
+    <div className={styles["mac-book-pro-16-14"]}>
+      
+      <div className={styles["shell"]}>
+        <div className="item">
+        
+          <svg
+          className={styles["group-107"]}
+          width="254"
+          height="329"
+          viewBox="0 0 254 329"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          >
+          <path
+            d="M253.114 30.6133C253.114 14.0448 239.683 0.613281 223.114 0.613281H30.0002C13.4316 0.613281 0.000183105 14.0447 0.000183105 30.6133V298.345C0.000183105 314.914 13.4316 328.345 30.0002 328.345H223.114C239.683 328.345 253.114 314.914 253.114 298.345V30.6133Z"
+            fill="#8A946E"
+          />
+          <path
+            d="M253.114 0.613281H57.5966V328.345H253.114V0.613281Z"
+            fill="#8A946E"
+          />
+          <path
+            d="M253.114 147.706H0.000167847V328.345H253.114V147.706Z"
+            fill="#8A946E"
+          />
+          </svg>
+
+          <svg
+            className={styles["group-108"]}
+            width="348"
+            height="329"
+            viewBox="0 0 348 329"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+          <path
+            d="M0.834961 30.6133C0.834961 14.0448 14.2664 0.613281 30.835 0.613281H317.456C334.025 0.613281 347.456 14.0447 347.456 30.6133V298.345C347.456 314.914 334.025 328.345 317.456 328.345H30.835C14.2664 328.345 0.834961 314.914 0.834961 298.345V30.6133Z"
+            fill="#8A946E"
+          />
+          <path
+            d="M0.834961 0.613281H268.582V328.345H0.834961V0.613281Z"
+            fill="#8A946E"
+          />
+          <path
+            d="M0.834961 147.706H347.456V328.345H0.834961V147.706Z"
+            fill="#8A946E"
+          />
+          </svg>
+          
+          <div className={styles["rectangle-72"]}></div>
+          <div className={styles["discover-more"]}>Discover more -&gt; </div>
+          <img className={styles["image"]} src="image-36.png" />
+          <div className={styles["description"]}>Open Garden Day </div>
+          </div>
+        
+        <div className="item">
+        <svg
+          className={styles["group-107"]}
+          width="254"
+          height="329"
+          viewBox="0 0 254 329"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          >
+          <path
+            d="M253.114 30.6133C253.114 14.0448 239.683 0.613281 223.114 0.613281H30.0002C13.4316 0.613281 0.000183105 14.0447 0.000183105 30.6133V298.345C0.000183105 314.914 13.4316 328.345 30.0002 328.345H223.114C239.683 328.345 253.114 314.914 253.114 298.345V30.6133Z"
+            fill="#8A946E"
+          />
+          <path
+            d="M253.114 0.613281H57.5966V328.345H253.114V0.613281Z"
+            fill="#8A946E"
+          />
+          <path
+            d="M253.114 147.706H0.000167847V328.345H253.114V147.706Z"
+            fill="#8A946E"
+          />
+          </svg>
+
+          <svg
+            className={styles["group-108"]}
+            width="348"
+            height="329"
+            viewBox="0 0 348 329"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+          <path
+            d="M0.834961 30.6133C0.834961 14.0448 14.2664 0.613281 30.835 0.613281H317.456C334.025 0.613281 347.456 14.0447 347.456 30.6133V298.345C347.456 314.914 334.025 328.345 317.456 328.345H30.835C14.2664 328.345 0.834961 314.914 0.834961 298.345V30.6133Z"
+            fill="#8A946E"
+          />
+          <path
+            d="M0.834961 0.613281H268.582V328.345H0.834961V0.613281Z"
+            fill="#8A946E"
+          />
+          <path
+            d="M0.834961 147.706H347.456V328.345H0.834961V147.706Z"
+            fill="#8A946E"
+          />
+          </svg>
+          
+          <div className={styles["rectangle-72"]}></div>
+          <div className={styles["discover-more"]}>Discover more -&gt; </div>
+          <img className={styles["image"]} src="image-37.png" />
+          <div className={styles["description"]}>Woodchop </div>
+        </div>
+        <div className="item">
+        <svg
+          className={styles["group-107"]}
+          width="254"
+          height="329"
+          viewBox="0 0 254 329"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          >
+          <path
+            d="M253.114 30.6133C253.114 14.0448 239.683 0.613281 223.114 0.613281H30.0002C13.4316 0.613281 0.000183105 14.0447 0.000183105 30.6133V298.345C0.000183105 314.914 13.4316 328.345 30.0002 328.345H223.114C239.683 328.345 253.114 314.914 253.114 298.345V30.6133Z"
+            fill="#8A946E"
+          />
+          <path
+            d="M253.114 0.613281H57.5966V328.345H253.114V0.613281Z"
+            fill="#8A946E"
+          />
+          <path
+            d="M253.114 147.706H0.000167847V328.345H253.114V147.706Z"
+            fill="#8A946E"
+          />
+          </svg>
+
+          <svg
+            className={styles["group-108"]}
+            width="348"
+            height="329"
+            viewBox="0 0 348 329"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+          <path
+            d="M0.834961 30.6133C0.834961 14.0448 14.2664 0.613281 30.835 0.613281H317.456C334.025 0.613281 347.456 14.0447 347.456 30.6133V298.345C347.456 314.914 334.025 328.345 317.456 328.345H30.835C14.2664 328.345 0.834961 314.914 0.834961 298.345V30.6133Z"
+            fill="#8A946E"
+          />
+          <path
+            d="M0.834961 0.613281H268.582V328.345H0.834961V0.613281Z"
+            fill="#8A946E"
+          />
+          <path
+            d="M0.834961 147.706H347.456V328.345H0.834961V147.706Z"
+            fill="#8A946E"
+          />
+          </svg>
+          
+          <div className={styles["rectangle-72"]}></div>
+          <div className={styles["discover-more"]}>Discover more -&gt; </div>
+          <img className={styles["image"]} src="image-38.png" />
+          <div className={styles["description"]}>Bakeoff </div>
+        </div>
+      </div>
+    </div>
+    
+  );
+};
